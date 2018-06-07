bd37e834342d5d2833b40575ca75dfb2a4728b40
Kevin <lzy12611@ly.com>
2018-06-07 16:06:58 +0800

    优化slider

--- slider/slider.js
+++ slider/slider.js
@@ -1,26 +1,27 @@
-function scrollSlide(obj){
-    var defaultObj={
-        startpoi:0,  //点击时的坐标
-        distancepoi:0,  //移动时的坐标
-        callback:function(){},  //回调
-        transitionTime:0.5,  //回调函数
-        intervalTime:3000,  //滑动间隔的时间
-        range:80,  //整屏滑动所需滑动距离
-        width:$('body').width(),  //单个容器的宽度
-        height:$('body').height() //单个容器的高度
+function scrollSlide(obj) {
+    var defaultObj = {
+        startpoi: 0,  //点击时的坐标
+        distancepoi: 0,  //移动时的坐标
+        callback: function () { },  //回调
+        transitionTime: 0.5,  //回调函数
+        intervalTime: 3000,  //滑动间隔的时间
+        range: 80,  //整屏滑动所需滑动距离
+        width: $('body').width(),  //单个容器的宽度
+        height: $('body').height() //单个容器的高度
     };
-    $.extend(this,defaultObj,obj);
+    $.extend(this, defaultObj, obj);
     this.totalcount = this.childDom.length;  //滑动页面数
-    this.length =this.horizontal?this.width:this.height; //垂直滑动则为高度，水平滑动则为宽度
-    this.count=!this.loop?(obj.count?((obj.count-1)%this.totalcount):0):(obj.count?(obj.count%this.totalcount==0?this.totalcount:obj.count%this.totalcount):1); //当前页面，默认从第一个开始
+    this.totalcount==1&&(this.loop=false);
+    this.length = this.horizontal ? this.width : this.height; //垂直滑动则为高度，水平滑动则为宽度
+    this.count = !this.loop ? (obj.count ? ((obj.count - 1) % this.totalcount) : 0) : (obj.count ? (obj.count % this.totalcount == 0 ? this.totalcount : obj.count % this.totalcount) : 1); //当前页面，默认从第一个开始
     this.init();
 }
-scrollSlide.prototype={
+scrollSlide.prototype = {
     /**
      *绑定事件
      */
-    bindEvents:function(){
-        var self=this;
+    bindEvents: function () {
+        var self = this;
         self.parentDom.on("touchstart", self.startFnc.bind(self));
         self.parentDom.on("touchmove", self.touchFnc.bind(self));
         self.parentDom.on("touchend", self.endFnc.bind(self));
@@ -28,125 +29,129 @@ scrollSlide.prototype={
     /**
      * 开始触摸的回调
      */
-    startFnc:function(event){
-        var self=this;
-        if(self.loop){
-            self.count==self.totalcount+1&&(self.count=1);
-            self.count==0&&(self.count=self.totalcount);
+    startFnc: function (event) {
+        var self = this,
+            touches = event.touches ? event.touches : event.originalEvent.touches;
+        if (self.loop) {
+            self.count == self.totalcount + 1 && (self.count = 1);
+            self.count == 0 && (self.count = self.totalcount);
         }
-        self.distancepoi=0; //滑动距离重置，防止点击后直接滑动
-        self.startpoi = self.horizontal?event.touches[0].pageX:event.touches[0].pageY;
+        self.distancepoi = 0; //滑动距离重置，防止点击后直接滑动
+        self.startpoi = self.horizontal ? touches[0].pageX : touches[0].pageY;
         self.movelength = -self.count * self.length;  //记录上次滑动距离
-        self.setCssStyle(self.movelength,0); //防止
+        self.setCssStyle(self.movelength, 0); //防止
     },
     /**
      * 触摸中的回调
      */
-    touchFnc:function(event){
-        var self=this;
-        event.preventDefault();
-        self.distancepoi = (self.horizontal?event.touches[0].pageX:event.touches[0].pageY) - self.startpoi;
-        if (!self.loop&&!self.resilience&&((self.count == 0 && self.distancepoi > 0) || (self.count == (self.totalcount - 1) && self.distancepoi < 0))) return; //上下两端拉动无效果,禁止回弹
-        self.setCssStyle(self.distancepoi + self.movelength,0);  //滑动中...
+    touchFnc: function (event) {
+        var self = this,
+            touches = event.touches ? event.touches : event.originalEvent.touches;
+        self.horizontal || event.preventDefault();
+        self.distancepoi = (self.horizontal ? touches[0].pageX : touches[0].pageY) - self.startpoi;
+        if (!self.loop && !self.resilience && ((self.count == 0 && self.distancepoi > 0) || (self.count == (self.totalcount - 1) && self.distancepoi < 0))) return; //上下两端拉动无效果,禁止回弹
+        self.setCssStyle(self.distancepoi + self.movelength, 0);  //滑动中...
     },
     /**
      * 触摸结束的回调
      */
-    endFnc:function(event){
-        var self=this,interimCount=self.count;
-        if(!self.loop){
-            if (self.distancepoi >= self.range && self.count > 0) self.count --; //下滑--右划
-            else if (self.distancepoi <= -self.range && self.count < self.totalcount - 1) self.count ++; //上划--左划
+    endFnc: function (event) {
+        var self = this, interimCount = self.count;
+        if (!self.loop) {
+            if (self.distancepoi >= self.range && self.count > 0) self.count--; //下滑--右划
+            else if (self.distancepoi <= -self.range && self.count < self.totalcount - 1) self.count++; //上划--左划
         }
         else {
-            if (self.distancepoi >= self.range && self.count > 0) self.count --;//下滑--右划
-            else if (self.distancepoi <= -self.range && self.count < self.totalcount+1) self.count ++; //上划--左划
+            if (self.distancepoi >= self.range && self.count > 0) self.count--;//下滑--右划
+            else if (self.distancepoi <= -self.range && self.count < self.totalcount + 1) self.count++; //上划--左划
         }
-        self.setCssStyle(-self.count * self.length,self.transitionTime); //实时滑动距离
-        if(self.auto&&self.loop){
+        self.setCssStyle(-self.count * self.length, self.transitionTime); //实时滑动距离
+        if (self.auto && self.loop) {
             clearTimeout(self.timer);
-            self.timer=setTimeout(self.play.bind(self),self.intervalTime);
+            self.timer = setTimeout(self.play.bind(self), self.intervalTime);
         }
-        interimCount!=self.count&&self.callbackloopType!=2&&self.callback(self.count,self.totalcount);
-        self.dot&&self.totalcount!=1&&self.changeDot();
+        interimCount != self.count && self.callbackloopType != 2 && self.callback(self.count, self.totalcount);
+        self.dot && self.totalcount != 1 && self.changeDot();
     },
     /**
      * 自动播放
      */
-    autoPlay:function(){
-        var self=this;
-        self.timer=setTimeout(self.play.bind(self),self.intervalTime);
+    autoPlay: function () {
+        var self = this;
+        self.timer = setTimeout(self.play.bind(self), self.intervalTime);
     },
     /**
      * 自动播放逻辑
      */
-    play:function (){
-        var self=this;
-        if(!self.reverse){  //自动上滑
-            self.count==self.totalcount+1&&(self.count=1)&&(self.setCssStyle(-self.count*self.length,0))
+    play: function () {
+        var self = this;
+        if (!self.reverse) {  //自动上滑
+            self.count == self.totalcount + 1 && (self.count = 1) && (self.setCssStyle(-self.count * self.length, 0))
             self.count++;
         }
         else {  //自动下滑
-            self.count==0&&(self.count=self.totalcount)&&(self.setCssStyle(-self.count*self.length,0));
+            self.count == 0 && (self.count = self.totalcount) && (self.setCssStyle(-self.count * self.length, 0));
             self.count--;
         }
-        setTimeout(function(){
-            self.setCssStyle(-self.count*self.length,self.transitionTime);
-            self.dot&&self.totalcount!=1&&self.changeDot();
+        setTimeout(function () {
+            self.setCssStyle(-self.count * self.length, self.transitionTime);
+            self.dot && self.totalcount != 1 && self.changeDot();
             clearTimeout(self.timer);
-            self.timer=setTimeout(self.play.bind(self),self.intervalTime);
-            self.callbackloopType!=1&&self.callback(self.count,self.totalcount);
-        },50)
+            self.timer = setTimeout(self.play.bind(self), self.intervalTime);
+            self.callbackloopType != 1 && self.callback(self.count, self.totalcount);
+        }, 50)
     },
     /**
      * 设置css样式
      * @param length
      * @param time
      */
-    setCssStyle:function(length,time){
-        var self=this;
+    setCssStyle: function (length, time) {
+        var self = this;
         self.parentDom.css({
-            'transform': !self.horizontal?('translate3d(0,' + length + 'px,0)'):('translate3d('+length+'px,0,0)'),
-            '-webkit-transform': !self.horizontal?('translate3d(0,' + length + 'px,0)'):('translate3d(' + length + 'px,0,0)'),
-            'transition': time+'s',
-            '-webkit-transition': time+'s'
+            'transform': !self.horizontal ? ('translate3d(0,' + length + 'px,0)') : ('translate3d(' + length + 'px,0,0)'),
+            '-webkit-transform': !self.horizontal ? ('translate3d(0,' + length + 'px,0)') : ('translate3d(' + length + 'px,0,0)'),
+            'transition': time + 's',
+            '-webkit-transition': time + 's'
         });
     },
     /**
      * 增加小圆点
      */
-    addDot:function(){
-        var self=this,dotHtml='<ul class="dot-list" ontouchmove="return false;">';
-        for(var i=0;i<self.totalcount;i++){
-            dotHtml+='<li class="dot"></li>';
+    addDot: function () {
+        var self = this, dotHtml = '<ul class="dot-list" ontouchmove="return false;">';
+        for (var i = 0; i < self.totalcount; i++) {
+            dotHtml += '<li class="dot"></li>';
         }
-        dotHtml+='</ul>';
+        dotHtml += '</ul>';
         self.outDom.append(dotHtml);
-        !self.loop?($(self.outDom.find('.dot')[self.count]).addClass('active')):($(self.outDom.find('.dot')[self.count-1]).addClass('active')); //此时的count为初始态count，标志着起初页面为第几个
+        !self.loop ? ($(self.outDom.find('.dot')[self.count]).addClass('active')) : ($(self.outDom.find('.dot')[self.count - 1]).addClass('active')); //此时的count为初始态count，标志着起初页面为第几个
     },
     /**
      * 初始化方法
      */
-    init:function(){
-        this.dot&&this.totalcount!=1&&this.addDot();
-        this.childDom.css({'height':this.height+'px','width':this.width+'px'});
-        this.outDom.css({'height':this.height+'px','width':this.width+'px'});
-        this.parentDom.css('width',(this.loop?this.width*(this.totalcount+2):(this.width*this.totalcount))+'px');
-        this.horizontal&&(this.parentDom.css({'display':'-webkit-box'}));
-        this.loop&& (this.parentDom.append(this.childDom[0].outerHTML))&&($(this.childDom[this.totalcount-1].outerHTML).insertBefore(this.childDom[0])); //可循环则新增一个元素
-        this.setCssStyle(-this.count * this.length,0);
-        this.firstCallback&&this.callback(this.count,this.totalcount);
-        this.loop&&this.auto&&this.totalcount!=1&&this.autoPlay();
-        !this.bantouch&&this.totalcount!=1&&this.bindEvents();
+    init: function () {
+        //初始化监听事件和圆点
+        this.parentDom.off();
+        this.outDom.find('.dot-list').remove();
+        this.dot && this.totalcount != 1 && this.addDot();
+        this.childDom.css({ 'height': this.height + 'px', 'width': this.width + 'px' });
+        this.outDom.css({ 'height': this.height + 'px', 'width': this.width + 'px' });
+        this.parentDom.css('width', (this.loop ? this.width * (this.totalcount + 2) : (this.width * this.totalcount)) + 'px');
+        this.horizontal && (this.parentDom.css({ 'display': '-webkit-box' }));
+        this.loop && (this.parentDom.append(this.childDom[0].outerHTML)) && ($(this.childDom[this.totalcount - 1].outerHTML).insertBefore(this.childDom[0])); //可循环则新增一个元素
+        this.setCssStyle(-this.count * this.length, 0);
+        this.firstCallback && this.callback(this.count, this.totalcount);
+        this.loop && this.auto && this.totalcount != 1 && this.autoPlay();
+        !this.bantouch && this.totalcount != 1 && this.bindEvents();
     }
     ,
     /**
      * 更新dot状态
      */
-    changeDot:function(){
-        var self=this,activeCount;
-        activeCount=self.loop?(self.count==0?(self.totalcount-1):(self.count==(self.totalcount+1)?0:self.count-1)):self.count;
+    changeDot: function () {
+        var self = this, activeCount;
+        activeCount = self.loop ? (self.count == 0 ? (self.totalcount - 1) : (self.count == (self.totalcount + 1) ? 0 : self.count - 1)) : self.count;
         $(self.outDom.find('.dot')[activeCount]).addClass('active').siblings().removeClass('active');
     }
 }
-var ScrollSlide=new scrollSlide({childDom:$('.slide-block'),parentDom:$('.slide-block-circle'),outDom:$('.slide-circle')});
